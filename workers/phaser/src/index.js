2b92de6b264448f7d2368cea48d7fa2ace289a16
Chong-U Lim <me@chongdashu.com>
2017-05-05 22:34:38 +0100

    Basic javascript client connection

---
+++ workers/phaser/src/index.js
@@ -0,0 +1,42 @@
+const sdk = require("spatialos_worker_sdk");
+
+
+let locatorParameters = new sdk.LocatorParameters();
+locatorParameters.projectName = "chongulim";
+locatorParameters.credentialsType = sdk.LocatorCredentialsType.LOGIN_TOKEN;
+locatorParameters.loginToken = {
+  token: sdk.DefaultConfiguration.LOCAL_DEVELOPMENT_LOGIN_TOKEN
+};
+
+
+let workerType = "PhaserClient";
+const connectionParameters = new sdk.ConnectionParameters();
+connectionParameters.workerType = workerType;
+
+
+const locator = sdk.Locator.create(sdk.DefaultConfiguration.LOCAL_DEVELOPMENT_LOCATOR_URL, locatorParameters);
+locator.getDeploymentList((err, deploymentList) => {
+  locator.connect("my_deployment", connectionParameters, (err, queueStatus) => {
+      return true;
+    },
+    (err, connection) => {
+      if (err) {
+        console.log("Error when connecting", err);
+        return;
+      }
+      connection.sendLogMessage(sdk.LogLevel.WARN, workerType, "Hello from JavaScript!");
+
+
+      let dispatcher = sdk.Dispatcher.create();
+      dispatcher.onDisconnect(op => {
+        console.log("---> Disconnected", op);
+      });
+      connection.attachDispatcher(dispatcher);
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Code which depends on the HTML DOM content.
+  console.log("Hello World!");
+});
